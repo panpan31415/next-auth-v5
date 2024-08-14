85023c493ddbbdebed0e4a9d090270156281dc63
panpan <panpan31415@gmail.com>
2024-08-14 17:11:18 +0200

    register function added

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,12 +1,32 @@
+"use server";
+import { getUserByEmail } from "@/lib/data";
+import prisma from "@/lib/db";
 import { RegisterFormValues } from "@/lib/types";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcryptjs";
 
 export async function register(values: RegisterFormValues) {
     const validatedFields = RegisterSchema.safeParse(values);
     if (!validatedFields.success) {
         return { error: "Invalid fields" };
     }
+    const { email, password, name } = validatedFields.data;
+
+    const existingUser = await getUserByEmail(email);
+    if (existingUser) {
+        return {
+            error: "Email is already in use!",
+        };
+    }
+
+    const pswHash = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+        data: { email, name, pswHash },
+    });
+    console.log("user is created", user);
+    // TODO: Send verification token email
+
     return {
-        success: "Register succeeded",
+        success: "User created!",
     };
 }
